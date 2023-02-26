42e11a136b72a4ea6218c75804a5502d5440738c
sanjiv576 <sanjiv576>
2023-02-26 13:12:20 +0545

    interface in dart

---
+++ oop_concept/8_interface.dart
@@ -0,0 +1,48 @@
+/*
+NOte: 'interface' keyword is not used in Dart as Java
+In Dart simply class can work as an interface.
+
+--> each method of interface must be overridden
+*/
+
+class Remote {
+  void volumeUp() {
+    print("Volume up in Remote");
+  }
+
+  void volumeDown() {
+    print("Volume down in Remote");
+  }
+}
+
+class AnotherInterface {
+  void forward() {
+    print("Forwarding from remote.");
+  }
+}
+
+// here, Remote class works as interface
+class Television implements Remote, AnotherInterface {
+  //  override method of Remote interface
+  void volumeUp() {
+    print("Volume up in Television.");
+  }
+
+  //  override method of Remote interface
+  void volumeDown() {
+    print("Volume down in Television.");
+  }
+
+  // override method of AnotherInterface
+  void forward() {
+    print("Forwarding from Televsion.");
+  }
+}
+
+void main() {
+  var tv = Television();
+  // super.volumeUp();  // is not allowed in interface
+  tv.volumeDown();
+  tv.volumeUp();
+  tv.forward();
+}
