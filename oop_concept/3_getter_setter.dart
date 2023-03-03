71a6d7b2caf1f42b36e19c0edeaa87f5eb1a0d33
sanjiv576 <sanjiv576>
2023-03-03 09:53:46 +0545

    added non-null assertion operator

--- oop_concept/3_getter_setter.dart
+++ oop_concept/3_getter_setter.dart
@@ -20,6 +20,36 @@ void main() {
   Student s1 = new Student();
   print("Default contact is : ${s1.contact}");
 
-  s1.contact = 981243;  // set the new contact--> calling setter method
-  print("New contact : ${s1.contact}");  // calling getter method
+  s1.contact = 981243; // set the new contact--> calling setter method
+  print("New contact : ${s1.contact}"); // calling getter method
+
+  Book book1 = Book("Comic", 123.45);
+  print(book1.name);
+  book1.name = "Science";
+  print("New Book name: ${book1.name}");
+}
+
+class Book {
+  // private fields
+  String? _name;
+  double? _price;
+
+// constructor
+  Book(this._name, this._price);
+
+/*
+In the getter methods name and price, ! is added to the end of the property name
+ to tell the compiler that we know the value is not null and we want to access it directly. 
+ This is called a non-null assertion operator. If the value is null, it will throw a runtime exception. 
+*/
+
+  // getter method to access name property
+  String get name => this._name!;
+
+  // getter methodto access price
+  double get price => this._price!;
+
+  // setter methods
+  set name(String name) => this._name = name;
+  set price(double price) => this._price = price;
 }
