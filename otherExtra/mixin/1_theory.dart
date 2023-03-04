bdc981685cde7058968b2201c6f50cf7c4d432fd
sanjiv576 <sanjiv576>
2023-03-04 09:27:17 +0545

    Mixin theory

---
+++ otherExtra/mixin/1_theory.dart
@@ -0,0 +1,17 @@
+/*
+Mixin is a way of resuing the code in multiple classes. 
+-> mixin keyword is used for declaration
+--> 3 keywords are used while working with it: mixin, with, on
+--> It is possible to use multiple mixins in a class.
+
+# Rules for Mixin
+--> cannot be instantiated (objects cannot be created)
+--> cannot be inherited/extended
+--> use the Mixin to share the common code for multiple classes
+--> has no constructor 
+
+# Allow in Dart
+--> You can add properties and static variables.
+--> You can add regular, abstract, and static methods.
+--> You can use one or more mixins in a class.
+*/
