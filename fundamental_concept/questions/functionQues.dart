47a0741b90b46b0f85aa18ffe85d883a27073b0e
sanjiv576 <sanjiv576>
2023-03-06 11:57:11 +0545

    add some functions

--- fundamental_concept/questions/functionQues.dart
+++ fundamental_concept/questions/functionQues.dart
@@ -26,7 +26,8 @@ String generateRandomPassword([int size = 8]) {
   String password = " ";
   for (int i = 0; i < size; i++) {
     // get random ASCI code from 33 to 126
-    int randomAsciCode = random.nextInt(94) + 33;  // here, 94 is passed as an argument instead of 126 because it generates 0 to 93 and then adds 33.
+    int randomAsciCode = random.nextInt(94) +
+        33; // here, 94 is passed as an argument instead of 126 because it generates 0 to 93 and then adds 33.
     // print("$randomAsciCode = ${String.fromCharCode(randomAsciCode)}");
     // convert ASCI code to a character and concatenate it
     password += String.fromCharCode(randomAsciCode);
@@ -34,6 +35,24 @@ String generateRandomPassword([int size = 8]) {
   return password;
 }
 
+double areaCircle(double radius) => radius * radius * 3.14;
+
+NthPowerOf(var num, int power) => pow(num, power);
+
+String reverse(String word) {
+  String reverseWord = "";
+  for (int i = word.length - 1; i >= 0; i--) {
+    reverseWord += word[i];
+  }
+  return reverseWord;
+}
+
+Function pythograusTheorem =
+    (var base, var perpendicular) => sqrt(pow(base, 2) + pow(perpendicular, 2));
+
 void main() {
   print("Your password is : ${generateRandomPassword(10)}");
+  print(reverse("HellO"));
+  print(NthPowerOf(3, 3));
+  print(pythograusTheorem(5, 5));
 }
