dd12cdb9f0ac4c7b9b79fc379218cdfc9436d20b
sanjiv576 <sanjiv576>
2023-03-21 20:22:21 +0545

    class for question set

---
+++ oop_concept/ques/quiz/question_bank.dart
@@ -0,0 +1,11 @@
+// Create a simple quiz application using oop that allows users to play and view their score.
+
+class QuestionBank {
+  String? question;
+  List<String>? options;
+  String? answer;
+
+  QuestionBank(
+      {required this.question, required this.options, required this.answer});
+}
+
