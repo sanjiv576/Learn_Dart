cefa2b0ac5529d0401d5d9b4be368e447732123c
sanjiv576 <shresthasanjiv576@gmail.com>
2023-03-25 09:22:35 +0545

    add some questions and answers

--- asynchronous/practice.dart
+++ asynchronous/practice.dart
@@ -1,12 +1,16 @@
 import 'dart:io';
+import 'dart:async';
+// import 'package:http/http.dart' as http;
 
 Future<void> main() async {
   print('start');
   getCurrentTime();
 
-  await for(String name in getMyFriendsName()){
+  await for (String name in getMyFriendsName()) {
     print(name);
   }
+
+  greet();
   print('end');
 }
 
@@ -28,3 +32,44 @@ Stream<String> getMyFriendsName() async* {
   await Future.delayed(Duration(seconds: 2));
   yield 'Santosh';
 }
+
+// Write a Dart function that uses a Future to print a message after a delay of 5 seconds.
+
+void greet() async {
+  var greetMsg = await printMessage();
+  print(greetMsg);
+}
+
+Future<String> printMessage() async {
+  late String message;
+  await Future.delayed(Duration(seconds: 5), () {
+    DateTime time = DateTime.now();
+    int hour = time.hour;
+
+    if (hour > 17) {
+      message = "Good Evening";
+    } else if (hour > 12) {
+      message = 'Good Afternoon';
+    } else {
+      message = 'Good Morning';
+    }
+  });
+  return message;
+}
+
+
+// Write a Dart program that uses async and await to perform two 
+//HTTP requests in parallel and print their responses to the console.
+
+
+
+// Future<void> main() async {
+//   final response1 = http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+//   final response2 = http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/2'));
+  
+//   final responses = await Future.wait([response1, response2]);
+  
+//   for (var response in responses) {
+//     print(response.body);
+//   }
+// }
